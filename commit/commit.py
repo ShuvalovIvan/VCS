ca2ef34e1b02f51ec5bc4da1d5732b6d4211eaba
Vignesh Saravanakumar <saravanakumar.vi@northeastern.edu>
2025-09-06 15:59:20 -0400

    Refactor commit function and update template structure

--- commit/commit.py
+++ commit/commit.py
@@ -1,4 +1,10 @@
+import os
+
+
 def cmd_commit(message=""):
     """Create a commit with optional message"""
     print("commit command called")
     print(f"  message: '{message}'")
+
+
+current_dir = os.getcwd()
