e8a765f6249d6af5e24c44ec151d4c4832c91fc7
Trate318 <ebeber318@gmail.com>
2025-09-06 16:34:54 -0400

    generate lcs matrix

--- diff/diff_class.py
+++ diff/diff_class.py
@@ -22,3 +22,17 @@ class Diff:
                 return [line.rstrip("\n\r") for line in f.readlines()]
         except FileNotFoundError:
             return []
+        
+    def longest_common_subsequence(self, file1_lines, file2_lines):
+        m = len(file1_lines)
+        n = len(file2_lines)
+
+        lcs_matrix = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if (file1_lines[i] == file2_lines[j]):
+                    lcs_matrix[i][j] = lcs_matrix[i - 1][j - 1] + 1
+                else:
+                    lcs_matrix[i][j] = max(lcs_matrix[i-1][j], lcs_matrix[i][j-1])
+        return lcs_matrix
