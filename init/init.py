6643a2785b856aebc6a04a82240f5abc80082015
Vignesh Saravanakumar <saravanakumar.vi@northeastern.edu>
2025-09-06 17:40:21 -0400

    Create TODOs

--- init/init.py
+++ init/init.py
@@ -2,28 +2,27 @@ import os
 import json
 from status.status import status
 
+
 def cmd_init():
     """Initialize a new repository"""
-    print("init command called")
 
     if status.repository_exists():
-        raise ValueError("Repository already exists")
-    
+        print("Repository already exists")
+        return
+
     current_dir = os.getcwd()
     os.makedirs(current_dir + "/.vcs/", exist_ok=True)
     os.makedirs(current_dir + "/.vcs/data", exist_ok=True)
 
     if not os.path.exists(current_dir + "/.vcs/main.json"):
-        with open(current_dir + "/.vcs/main.json", "w", encoding='utf-8') as f:
+        with open(current_dir + "/.vcs/main.json", "w", encoding="utf-8") as f:
             json.dump({}, f)
     else:
         print("Repository exists")
     if not os.path.exists(current_dir + "/.vcs/data/status.json"):
-        with open(current_dir + "/.vcs/data/status.json", "w", encoding='utf-8') as f:
+        with open(current_dir + "/.vcs/data/status.json", "w", encoding="utf-8") as f:
             json.dump({"branch": "main"}, f)
     else:
         print("Data status exists")
 
-    
-
-    
+    print("repository initialized")
