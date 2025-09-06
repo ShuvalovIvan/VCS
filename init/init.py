620d4bd662833003ac549be9f5b7e6da9f47e1f2
IvanShuvalov <vanyashuvalov@gmail.com>
2025-09-06 15:40:45 -0400

    Wrote Init cmd

--- init/init.py
+++ init/init.py
@@ -1,3 +1,18 @@
+import os
+
 def cmd_init():
     """Initialize a new repository"""
     print("init command called")
+    current_dir = os.getcwd()
+    os.makedirs(current_dir + "/.vcs/objects", exist_ok=True)
+    os.makedirs(current_dir + "/.vcs/refs/heads", exist_ok=True)
+
+    with open(current_dir + ".vcs/HEAD", "w") as f:
+        f.write("ref: refs/heads/main\n")
+
+    open(current_dir + "/.vcs/index", "w").close()
+    open(current_dir + "/.vcs/config", "w").close()
+    open(current_dir + "/.vcs/description", "w").close()
+    open(current_dir + "/.vcs/refs/heads/main", "w").close()
+
+    print(f"Initialized empty VCS repository in {current_dir}/.vcs/")
