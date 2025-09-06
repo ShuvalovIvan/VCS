76608219e60f47d2f06bc98ceb95dc36cc4da020
Vignesh Saravanakumar <saravanakumar.vi@northeastern.edu>
2025-09-06 16:11:42 -0400

    Check if repository exists before commiting

--- commit/commit.py
+++ commit/commit.py
@@ -1,8 +1,14 @@
 import os
 from status.status import status
 
+
 def cmd_commit(message=""):
     """Create a commit with optional message"""
+
+    if not status.repository_exists():
+        print("Error: No VCS repository found. Please run 'init' first.")
+        return
+
     print("commit command called")
     print(f"  message: '{message}'")
 
